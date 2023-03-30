6547813983c6c47fa5a0e32e8baf4bb56f2231be
VladDyadenko <Stilmagroup@gmail.com>
2023-03-30 21:09:04 +0300

    hw5_6

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,26 @@
 import { Outlet, NavLink,useLocation  } from 'react-router-dom';
 import { Suspense } from 'react';
+import styled from "styled-components";
 import Loader from 'components/Loader';
 
+const StyledLink = styled(NavLink)`
+  color: black;
+  text-decoration: none;
+  margin-right: 10px;
+  font-weight:500;
+
+  &.active {
+    color: red;
+  }
+`;
 
 const Header = () => {
   const location = useLocation();
   return (
     <div>
       <header>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies" state={{ from: location }}>Movies</NavLink>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/movies" state={{ from: location }}>Movies</StyledLink>
       </header>
       <Suspense fallback={<Loader/>}>
         <Outlet />
